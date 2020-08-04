0d6508cf5cdac53a1ee0659e4f32ab81a0d055d8
Nandhabalan Marimuthu <67896802+nandha-marimuthu@users.noreply.github.com>
2020-08-04 14:25:28 +0530

    Add files via upload

---
+++ sq_root.js
@@ -0,0 +1,10 @@
+function sq_root(n){
+    if (n<1){
+        console.log('Input is not valid')
+    }
+    else {
+        console.log("Square root of "+n+" is "+Math.sqrt(n))
+    }
+
+}
+sq_root(-64)
